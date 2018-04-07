b095fb401d54453dc3e9abd5b191b25fc1a63576
Zhehao Lu <zhehaolu@usc.edu>
2018-04-06 22:16:35 -0700

    Using javascript to replace some repeated (and very likely to be changed) html text

---
+++ final_project/repeatContent.js
@@ -0,0 +1,6 @@
+function copyright() {
+  document.write('<div id="copyright">\u00A9 Zhehao Lu 2018 @USC, no rights reserved</div>');
+}
+function navBar() {
+  document.write('<div id="nav"><div class="navItem"><a href="home.html">Home</a></div><div class="navItem"><a href="projects.html">Projects</a></div><div class="navItem"><a href="info.html">Personal Info</a></div><div class="navItem"><a href="misc.html">Misc</a></div><div class="clearFloat"></div></div>');
+}
