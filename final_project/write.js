5a22f67b552f77ba0804c99c9bd14b7089af7a1f
Zhehao Lu <zhehaolu@usc.edu>
2018-04-24 01:40:24 -0700

    Reformating pages. Added titles.

--- final_project/write.js
+++ final_project/write.js
@@ -2,18 +2,20 @@ function copyright() {
   document.write('<div id="copyright">\u00A9 Zhehao Lu 2018 @USC, no rights reserved</div>');
 }
 function navBar() {
-  document.write('\
-    <div class="codeFont" id="nav">\
+  document.write(
+    '<div class="codeFont" id="nav">\
       <div class="navItem"><a href="index.html">Home</a></div>\
       <div id="projects" class="navItem">\
         <a href="projects.html">Projects</a>\
         <div id="dropdown-content">\
-          <a>Something</a><br/>\
-          <a>Something Else</a>\
+          <a href="stellar.html">Stellar</a><br/>\
+          <a href="overryde.html">Overryde</a><br/>\
+          <a href="librarian.html">Librarian</a><br/>\
+          <a href="ligo.html">LIGO</a><br/>\
         </div>\
       </div>\
       <div class="navItem"><a href="info.html">Personal Info</a></div>\
       <div class="navItem"><a href="misc.html">Misc</a></div><div class="clearFloat"></div>\
-    </div>\
-  ');
+    </div>'
+  );
 }
