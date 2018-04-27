ad916ab1689e6ad52d3afe69503e9d749afb89be
Zhehao Lu <zhehaolu@usc.edu>
2018-04-27 15:44:57 -0700

    Added logo

--- final_project/write.js
+++ final_project/write.js
@@ -1,9 +1,10 @@
 function copyright() {
   document.write('<div id="copyright">\u00A9 Zhehao Lu 2018 @USC, no rights reserved</div>');
 }
+
 function navBar() {
-  document.write(
-    '<div class="codeFont" id="nav">\
+  document.write('\
+    <div class="codeFont" id="nav">\
     <div class="navItem"><a href="index.html">Home</a></div>\
     <div class="navItem"><a href="info.html">About</a></div>\
       <div id="projects" class="navItem">\
@@ -18,6 +19,14 @@ function navBar() {
       <div class="navItem"><a href="misc.html">Misc</a></div>\
       <div class="navItem"><a href="contact.html">Contact</a></div>\
       <div class="clearFloat"></div>\
-    </div>'
-  );
+    </div>\
+  ');
+}
+
+function logo() {
+  document.write('\
+    <div id="logo">\
+      <a href="index.html"><img src="logo.jpg"><a>\
+    </div>\
+  ');
 }
